d83568addf0ef57ded9e6912a76f648bd16e48df
Pedro Leale <pedroleale@gmail.com>
2023-02-09 14:57:28 -0300

    First commit

---
+++ hybrid_data_structure/src/hybrid_data_structure.rs
@@ -0,0 +1,80 @@
+use bloomfilter::Bloom; // bloomfilter = "1.0.9"
+use std::collections::BTreeSet;
+use std::collections::btree_set::SymmetricDifference;
+
+pub struct Hybrid <'a> {
+    bloom: Bloom<&'a str>,
+    set: BTreeSet<&'a str>,
+}
+
+impl <'a> Hybrid <'a> {
+    pub fn new(items_count: usize) -> Hybrid <'a> {
+        Hybrid {
+            bloom: Bloom::new_for_fp_rate(items_count, 0.01),
+            set: BTreeSet::new(),
+        }
+    }
+
+    pub fn insert(&mut self, item: &'a str) {
+        self.bloom.set(&item);
+        self.set.insert(item);
+    }
+
+    pub fn contains(&self, item: &'a str) -> bool {
+        if self.bloom.check(&item) {
+            return self.set.contains(item);
+        }
+        false
+    }
+
+    pub fn get_item(&self, item: &'a str) -> Option<&'a str> {
+        if self.bloom.check(&item) {
+            return self.set.get(&item).copied();
+        }
+        None
+    }
+
+    pub fn iter(&self) -> std::collections::btree_set::Iter<&str> {
+        self.set.iter()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.set.is_empty()
+    }
+
+    pub fn is_subset(&self, other: &Hybrid) -> bool {
+        self.set.is_subset(&other.set)
+    }
+
+    pub fn is_superset(&self, other: &Hybrid) -> bool {
+        self.set.is_superset(&other.set)
+    }
+
+    pub fn is_disjoint(&self, other: &Hybrid) -> bool {
+        self.set.is_disjoint(&other.set)
+    }
+
+    pub fn symmetric_difference(&self, other: &'a Hybrid) ->  SymmetricDifference<'_, &str> {
+        return self.set.symmetric_difference(&other.set);
+    }
+
+    pub fn len(&self) -> usize {
+        self.set.len()
+    }
+
+    pub fn get_set(&self) -> &BTreeSet<&'a str> {
+        &self.set
+    }
+
+    pub fn get_set_mut(&mut self) -> &mut BTreeSet<&'a str> {
+        &mut self.set
+    }
+
+    pub fn get_bloom(&self) -> &Bloom<&'a str> {
+        &self.bloom
+    }
+
+    pub fn get_bloom_mut(&mut self) -> &mut Bloom<&'a str> {
+        &mut self.bloom
+    }
+}
---
+++ hybrid_data_structure/src/main.rs
@@ -0,0 +1,8 @@
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, Write};
+use std::path::Path;
+
+mod hybrid_data_structure;
+fn main() {
+    //TODO: test all functions from Hybrid
+}
