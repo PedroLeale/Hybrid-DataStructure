bdf49d5b64c67744000ed8c2b003e464d557c58b
Pedro Leale <pedroleale@gmail.com>
2023-03-06 23:44:31 -0300

    feature: List of structures
    Made a Hybrid structure containing a List of HybridBase for optimizing unions

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ Eventually this project will become a external crate at crates.io to be used ope
 
 It is a hybrid data structure that contains a BtreeSet and a BloomFilter.
 The BloomFilter's goal is to optimize "contains" methods without compromising insertions.<br />
-The current code is still on the optimization process.<br />
+The current code is still on the optimization process but is relatively fast.<br />
 
-## Future updates:
-To further optimize unions I am working on a way to store a list of the hybrid data structure, so in the case of an union of two big sets, a push
-to the end of the list can solve it. My current problem with this solution is to make iterators work properly.<br />
+## For the future updates:
+<del> To further optimize unions I am working on a way to store a list of the hybrid data structure, so in the case of an union of two big sets, a push
+to the end of the list can solve it. My current problem with this solution is to make iterators work properly. </del> --- DONE <br />
+Make a better way of concatenating the iterators, because of borrowing issues, otherwise from my testings it works.<br />
+Make a function that returns iterators with the intersection itself, for the heuristics just a boolean value is enough, but I want it to make this structure more useful for other cases.<br />
 I also want to test other Tree sets so the user can choose the best solution for the case.
--- hybrid_data_structure/src/hybrid_data_structure.rs
+++ hybrid_data_structure/src/hybrid_data_structure.rs
@@ -1,113 +1,137 @@
 use bloomfilter::Bloom; // bloomfilter = "1.0.9"
 use std::collections::BTreeSet;
-use std::collections::btree_set::SymmetricDifference;
-use std::collections::btree_set::Iter;
-
-pub struct Hybrid {
-    bloom: Bloom<String>,
-    set: BTreeSet<String>,
+struct HybridBase<'a> {
+    bloom: Bloom<str>,
+    set: BTreeSet<&'a str>,
 }
 
-impl <'a> Hybrid {
-    pub fn new(items_count: usize, fp_rate: f64) -> Hybrid {
+pub struct Hybrid <'a>  {
+    base: Vec<HybridBase<'a>>,
+}
+
+impl <'a> Hybrid <'a>{
+    pub fn new(items_count: usize, fp_rate: f64) -> Hybrid <'a> {
         Hybrid {
-            bloom: Bloom::new_for_fp_rate(items_count, fp_rate),
-            set: BTreeSet::new(),
+            base: vec![HybridBase {
+                bloom: Bloom::new_for_fp_rate(items_count, fp_rate),
+                set: BTreeSet::new(),
+            }],
         }
     }
 
-    pub fn insert(&mut self, item: String) {
-        self.bloom.set(&item);
-        self.set.insert(item);
+    pub fn insert(&mut self, item: &'a str) {
+        //Always insert on the last one
+        //Can be changed later
+        self.base.last_mut().unwrap().bloom.set(item);
+        self.base.last_mut().unwrap().set.insert(item);
     }
 
-    pub fn contains(&self, item: &String) -> bool {
-        if self.bloom.check(&item) {
-            return self.set.contains(item);
+    pub fn contains(&self, item: &'a str) -> bool {
+        //Start checking from last to first, because most insertions happens on the last position
+        //Will only impact if a union has occurred 
+        for base in self.base.iter().rev() { 
+            if base.bloom.check(item) {
+                return base.set.contains(item);
+            }
         }
         false
     }
 
-    pub fn get_item(&self, item: String) -> Option<&String> {
-        if self.bloom.check(&item) {
-            return self.set.get(&item).clone();
+    pub fn get_item(&self, item: &'a str) -> Option<&'a str> {
+        for base in self.base.iter().rev(){
+            if base.bloom.check(&item) {
+                return base.set.get(item).copied();
+            }
         }
         None
     }
-
-    pub fn iter(&self) -> std::collections::btree_set::Iter<String> {
-        self.set.iter()
+    
+    pub fn iter(&'a self) -> impl Iterator<Item = &'a &'a str> {
+        self.base
+            .iter()
+            .flat_map(|base| base.set.iter())
     }
 
-    pub fn iterate(&self, iter: fn(&String)) {
-        // A function that will iterate throught the set and call the function iter on each item
-        for item in self.set.iter() {
-            iter(item);
-        }
+    pub fn get_iter(&self) -> impl Iterator<Item = &str> + '_ {
+        self.base.iter().flat_map(|base| base.set.iter().map(|s| *s))
     }
 
     pub fn is_empty(&self) -> bool {
-        self.set.is_empty()
+        self.base.is_empty()
     }
 
+    //Optimize is_subset, is_superset and is_disjoint
     pub fn is_subset(&self, other: &Hybrid) -> bool {
-        self.set.is_subset(&other.set)
+        //Check if all items in self are in other
+        for item in self.get_iter() {
+            if !other.contains(item) {
+                return false;
+            }
+        }
+        true
     }
 
     pub fn is_superset(&self, other: &Hybrid) -> bool {
-        self.set.is_superset(&other.set)
+        //Check if all items in other are in self
+        for item in other.get_iter() {
+            if !self.contains(item) {
+                return false;
+            }
+        }
+        true
     }
 
     pub fn is_disjoint(&self, other: &Hybrid) -> bool {
-        self.set.is_disjoint(&other.set)
-    }
-
-    pub fn symmetric_difference(&'a self, other: &'a Hybrid) ->  SymmetricDifference<'_, String> {
-        return self.set.symmetric_difference(&other.set);
-    }
-
-    pub fn intersection(&'a self, other: &'a Hybrid) -> std::collections::btree_set::Intersection<'_, String> {
-        return self.set.intersection(&other.set);
-    }
-
-    pub fn union(&'a self, other: &'a Hybrid) -> std::collections::btree_set::Union<'_, String> {
-        return self.set.union(&other.set);
-    }
-
-    pub fn hybrid_union(&'a mut self, other: &'a mut Hybrid) -> &Hybrid { //Needs to be optimized
-        // If self is smaller, then insert all items from self into "other" and return other, else do the opposite
-        if self.len() < other.len() {
-            let new_hybrid = other;
-            for item in self.iter() {
-                new_hybrid.insert(item.to_string());
+        //Check if there is no intersection between self and other
+        for item in self.get_iter() {
+            if other.contains(item) {
+                return false;
             }
-            return new_hybrid;
-        } else {
-            let new_hybrid = self;
-            for item in other.iter() {
-                new_hybrid.insert(item.to_string());
-            }
-            return new_hybrid;
         }
+        true
+    }
+
+    pub fn has_intersection(&'a self, other: &'a Hybrid) -> bool {
+        //Returns true if an intersection exists, false if not
+        for item in self.get_iter() {
+            if other.contains(item) {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn union(&mut self, other: &'a mut Hybrid<'a>) -> &mut Hybrid<'a> {
+        //Makes "other" part of "self", if other is empty, nothing happens
+        //If other's length is too small, it will be inserted in a loop
+        //Else everything will be pushed to the list.
+        //"too small" will be altered after more benchmarking.
+        if other.is_empty() {
+            return self;
+        }
+        if other.len() <= 1000 {
+            for item in other.get_iter() {
+                self.insert(item);
+            }
+            return self;
+        }
+        loop {
+            let safe_check = other.base.pop();
+            if let Some(_) = safe_check {
+                self.base.push(safe_check.unwrap());
+            } else {
+                break;
+            }
+        }
+        self
     }
 
     pub fn len(&self) -> usize {
-        self.set.len()
+        let mut len = 0;
+        for base in self.base.iter() {
+            len += base.set.len();
+        }
+        len
     }
 
-    pub fn get_set(&self) -> &BTreeSet<String> {
-        &self.set
-    }
-
-    pub fn get_set_mut(&mut self) -> &mut BTreeSet<String> {
-        &mut self.set
-    }
-
-    pub fn get_bloom(&self) -> &Bloom<String> {
-        &self.bloom
-    }
-
-    pub fn get_bloom_mut(&mut self) -> &mut Bloom<String> {
-        &mut self.bloom
-    }
 }
